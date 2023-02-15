c8e226a092e8f879d4cd8250a6ed7b88e5fd4d3d
Velmeri <danielmikhh2gmail.com>
2023-02-15 10:07:02 +0100

    Добавьте файлы проекта.

---
+++ HW_150223/Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_150223
+{
+    internal class Book : IComparable, IComparer, ICloneable
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int PublicationYear { get; set; }
+
+        public Book() { }
+
+        public Book(string title, string author, int publicationYear)
+        {
+            Title = title;
+            Author = author;
+            PublicationYear = publicationYear;
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+
+            Book book = obj as Book;
+            if (book != null)
+                return this.Title.CompareTo(book);
+            else
+                throw new ArgumentException("Obj isn't a Book!!!");
+        }
+
+        public int Compare(object x, object y)
+        {
+            Book book1 = x as Book;
+            Book book2 = y as Book;
+
+            if (book1 == null || book2 == null)
+                throw new ArgumentException("Obk isn't a Book");
+
+            return this.Title.CompareTo(book1.Title);
+        }
+
+        public object Clone()
+        {
+            return new Book(this.Title, this.Author, this.PublicationYear);
+        }
+    }
+}
---
+++ HW_150223/Library.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_150223
+{
+    internal class Library : IEnumerable, IEnumerator
+    {
+        private Book[] books;
+        private int currentIndex = -1;
+
+        public Library(Book[] books)
+        {
+            this.books = books;
+        }
+
+        public object Current => throw new NotImplementedException();
+
+        public IEnumerator GetEnumerator()
+        {
+            return (IEnumerator) new LibraryEnumerator(this.books);
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        private class LibraryEnumerator : IEnumerator
+        {
+            private Book[] books;
+            private int currentIndex = -1;
+
+            // Constructor with array of books
+            public LibraryEnumerator(Book[] books)
+            {
+                this.books = books;
+            }
+
+            // Implementation of IEnumerator interface
+            public bool MoveNext()
+            {
+                this.currentIndex++;
+                return (this.currentIndex < this.books.Length);
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+
+            public object Current
+            {
+                get
+                {
+                    if (this.currentIndex >= 0 && this.currentIndex < this.books.Length)
+                        return this.books[this.currentIndex];
+                    else
+                        throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
